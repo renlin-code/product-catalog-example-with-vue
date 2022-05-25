a86d6dd3100a5c935bd811714d960267d8cb032c
renlin-code <renelj1997@gmail.com>
2022-05-25 22:53:40 +0500

    Maked fetchData function for rendering products´ templates with v-for directive

---
+++ src/components/HomeComp.vue
@@ -0,0 +1,39 @@
+<template>
+        <main class="grid container">
+        <div class="sub_category_page">
+            <div class="column_right column_right_products_container">
+                <div class="product__area">
+                    <ProductComp
+                        :key="product.productId" 
+                        v-for="product in products"
+                    />
+                </div>
+            </div>
+        </div>
+    </main>
+
+</template>
+
+<script>
+import ProductComp from "./ProductComp.vue";
+
+
+export default {
+    components: {
+        ProductComp
+    },
+    data() {
+        return {
+            products: [],
+        }
+    },
+    created() {
+        const fetchData = async () => {
+            const response = await fetch("./products.json");
+            this.products = await response.json();
+        };
+
+        fetchData();
+    }
+}
+</script>
